d1c1c17958957c5ddf897ef57be9f9b1dfac1925
Esakki Muthu <esakkimuthu6842@gmail.com>
2024-02-11 22:31:26 +0530

    Intial TypeScript Setup

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./App.css";
+// let name: string = "something";
+
+let someone: boolean = true;
+// console.log(someone);
+// let open: string[];
+// open = ["sdnjkdls", "sdjkds"];
+
+// type Person = {
+//   name: string;
+//   id?: number;
+// };
+
+// let person: Person = { name: "Esakki Muthu", id: 21 };
+// let bharu: Person = { name: "Bharathiii ", id: 123 };
+// console.log(bharu.name);
+// let somePerson: Person = { name: "someone", id: 278 };
+// let som: Person = { name: "smjjk" };
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1> Hello TypeScript!! ... </h1>
+    </div>
+  );
+};
+
+export default App;
